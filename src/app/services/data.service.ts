c41392a904f192a457e7ed6a973873186825adad
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2024-02-15 15:17:41 +0100

    fix: :fire: Update DataService and environment.ts, remove demo.js and Message.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,41 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-  
+
 export class DataService {
 
   fecha = new Date();
-  año = this.fecha.getFullYear() 
+  año = this.fecha.getFullYear()
   mes = this.fecha.getMonth() + 1;
   dia = this.fecha.getDay();
   fechaActual: string = ''
-  
+
 private uri = `${environment.urlApi}/es/datos/mercados/precios-mercados-tiempo-real?start_date=${this.getfechaActual()}T00:00&end_date=${this.getfechaActual()}T23:59&time_trunc=hour`
 
   constructor(private http: HttpClient) {}
 
   public getApi(): Observable <any> {
-    const options = {
-      headers: new HttpHeaders({
-        "Accept":" application/json",
-        "Content-Type": "application/json",
-        'Access-Control-Allow-Origin': '*',
-    }),
-    };
-    return this.http.get<any>(this.uri, options);
-
+    return this.http.get<any>(this.uri).pipe(catchError(this.handleError));
   }
-  getfechaActual():string {
 
-    this.fechaActual = (this.año + '-' + this.mes + '-' + this.dia)
-    return this.fechaActual; 
+  getfechaActual(): string {
+    return new Date().toISOString().slice(0, 10);;
   }
-  
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('melón mírate la url que te paso otra vez', error.error.message);
+    return throwError( ()=> new Error(`Algo salió mal ${error.message}`));
+  }
 
 }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false, 
-  urlApi : '/api'
+  production: false,
+  urlApi : 'https://apidatos.ree.es'
 };
 
 /*
